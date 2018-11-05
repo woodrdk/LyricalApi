84b6e224eec7989f4c84462a3b54363a6a3280fa
Rob Wood <rdrwood@gmail.com>
2018-11-05 14:57:33 -0800

    Source of lyrics.ovh and API to search for lyrics of a song https://lyrics.ovh

--- LyricalApi/Form1.Designer.cs
+++ LyricalApi/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LyricalApi
 {
-    partial class Form1
+    partial class frmMain
     {
         /// <summary>
         /// Required designer variable.
@@ -28,13 +28,145 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMain));
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.lblArtist = new System.Windows.Forms.Label();
+            this.lblSong = new System.Windows.Forms.Label();
+            this.txtArtistName = new System.Windows.Forms.TextBox();
+            this.txtSongTitle = new System.Windows.Forms.TextBox();
+            this.txtLyrics = new System.Windows.Forms.TextBox();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Location = new System.Drawing.Point(376, 40);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(84, 52);
+            this.btnSearch.TabIndex = 0;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // lblArtist
+            // 
+            this.lblArtist.AutoSize = true;
+            this.lblArtist.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblArtist.Location = new System.Drawing.Point(28, 36);
+            this.lblArtist.Name = "lblArtist";
+            this.lblArtist.Size = new System.Drawing.Size(117, 24);
+            this.lblArtist.TabIndex = 1;
+            this.lblArtist.Text = "Artist Name";
+            // 
+            // lblSong
+            // 
+            this.lblSong.AutoSize = true;
+            this.lblSong.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSong.Location = new System.Drawing.Point(28, 67);
+            this.lblSong.Name = "lblSong";
+            this.lblSong.Size = new System.Drawing.Size(105, 24);
+            this.lblSong.TabIndex = 2;
+            this.lblSong.Text = "Song Title";
+            // 
+            // txtArtistName
+            // 
+            this.txtArtistName.Location = new System.Drawing.Point(164, 40);
+            this.txtArtistName.Name = "txtArtistName";
+            this.txtArtistName.Size = new System.Drawing.Size(194, 20);
+            this.txtArtistName.TabIndex = 3;
+            // 
+            // txtSongTitle
+            // 
+            this.txtSongTitle.Location = new System.Drawing.Point(164, 72);
+            this.txtSongTitle.Name = "txtSongTitle";
+            this.txtSongTitle.Size = new System.Drawing.Size(194, 20);
+            this.txtSongTitle.TabIndex = 4;
+            // 
+            // txtLyrics
+            // 
+            this.txtLyrics.Location = new System.Drawing.Point(28, 109);
+            this.txtLyrics.Multiline = true;
+            this.txtLyrics.Name = "txtLyrics";
+            this.txtLyrics.ReadOnly = true;
+            this.txtLyrics.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.txtLyrics.Size = new System.Drawing.Size(432, 270);
+            this.txtLyrics.TabIndex = 5;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(28, 394);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 6;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(496, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(496, 429);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.txtLyrics);
+            this.Controls.Add(this.txtSongTitle);
+            this.Controls.Add(this.txtArtistName);
+            this.Controls.Add(this.lblSong);
+            this.Controls.Add(this.lblArtist);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.menuStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "Lyric Search Form";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Label lblArtist;
+        private System.Windows.Forms.Label lblSong;
+        private System.Windows.Forms.TextBox txtArtistName;
+        private System.Windows.Forms.TextBox txtSongTitle;
+        private System.Windows.Forms.TextBox txtLyrics;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
     }
 }
 
--- LyricalApi/Form1.cs
+++ LyricalApi/Form1.cs
@@ -4,17 +4,62 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace LyricalApi
 {
-    public partial class Form1 : Form
+    public partial class frmMain : Form
     {
-        public Form1()
+        HttpClient client = new HttpClient();
+
+        public frmMain()
         {
             InitializeComponent();
+
+            client.BaseAddress = 
+                new Uri("https://api.lyrics.ovh/v1/"); // must add forward slash at the end of base address
+            // Telling service how we want to accept data and other configuration options.
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/json"));
+            
+        }
+               
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string artist = txtArtistName.Text;
+            string songTitle = txtSongTitle.Text;
+
+            // note https://api.lyrics.ovh/v1/artistName/songTitle
+            HttpResponseMessage response = client.GetAsync($"{artist}/{songTitle}").Result;
+
+            if (response.IsSuccessStatusCode)
+            {
+                string output = response.Content.ReadAsStringAsync().Result;
+                //MessageBox.Show(output);
+
+                txtLyrics.Text = output.Replace("\\n", Environment.NewLine); // need to clean up so that it puts the new line on the new lines
+            }
+            else
+            {
+                txtLyrics.Text = "Artist or song not found";
+                //MessageBox.Show("Not Found");
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
         }
     }
+
 }
